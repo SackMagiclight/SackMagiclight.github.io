4a44a5112c4b654ee4a6fbf1175412706f057532
SackMagiclight <SackMagiclight@users.noreply.github.com>
2021-10-11 20:26:41 +0000

    deploy: 9f2ba38b90d819f962f0fe8b81a8ac26b0342bbb

--- _nuxt/static/1633983185/blog/items/rtx2060-reeked/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1633983185",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fitems\u002Frtx2060-reeked",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"252f478f"}},globalRefs:{}}}(null));
