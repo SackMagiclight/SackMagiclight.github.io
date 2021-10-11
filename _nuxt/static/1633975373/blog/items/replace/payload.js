1e905ad70273b4fe9d448803b52519f1eb92500c
SackMagiclight <SackMagiclight@users.noreply.github.com>
2021-10-11 19:05:18 +0000

    deploy: 365c131ad6f28cfab697504d1995851186cd5943

--- _nuxt/static/1633975373/blog/items/replace/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/blog/items/replace", {data:[{}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1633979111/blog/items/beatoraja-0-5-2/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1633979111",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fitems\u002Fbeatoraja-0-5-2",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"afb50228"}},globalRefs:{}}}(null));
