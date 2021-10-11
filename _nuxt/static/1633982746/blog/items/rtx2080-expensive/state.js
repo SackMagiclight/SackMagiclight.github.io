310325a64b45428b405ab98458ac50ee1a0e64be
SackMagiclight <SackMagiclight@users.noreply.github.com>
2021-10-11 20:13:12 +0000

    deploy: 7b6d30718e9bb0fc27f236273a7c750af8b8e7b0

--- _nuxt/static/1633982746/blog/items/rtx2080-expensive/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1633982746",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fitems\u002Frtx2080-expensive",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"903fc800"}},globalRefs:{}}}(null));
