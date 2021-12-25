bfabb14f10c6724659c215e0c84d49931cf2c9d7
SackMagiclight <20876249+SackMagiclight@users.noreply.github.com>
2021-12-25 19:15:50 +0900

    fix routing

--- pages/_year/_month/_day/_slug.vue
+++ pages/_year/_month/_day/_slug.vue
@@ -3,14 +3,17 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, useRoute, useRouter } from '@nuxtjs/composition-api'
+import { defineComponent, useMeta } from '@nuxtjs/composition-api'
 export default defineComponent({
     setup() {
-        const $router = useRouter()
-        const $route = useRoute()
-        const slug = $route.value.params.slug
-
-        $router.push(`/blog/items/${slug}/`)
+        const { script } = useMeta()
+        script.value = [
+            {
+                src: '/redirect.js'
+            }
+        ]
     },
+
+    head: {},
 })
 </script>
